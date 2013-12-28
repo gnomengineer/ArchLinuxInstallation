f5d57dd2578ef2467f3905417c144f749fa66342
Troopa <troopa.der.pate@gmail.com>
2013-12-29 00:13:43 +0100

    improvement of code structure

--- pacinstall.sh
+++ pacinstall.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-
-#author: Daniel Föhn aka (Don) Troopa
+#author: Daniel Foehn aka (Don) Troopa
 #date: 2013-11-28
 #desc: a script that installs package from a given list.
 #call:	pacinstall package_list.txt 
@@ -11,10 +10,10 @@ logfile="$folder/pacinstall.log"
 
 ### START LOG FILE HEADER ###
 { echo "log file for bash script 'pacinstall'"
-echo "last script runtime: $(date %c)"
+echo "last script runtime: $(date +%c)"
 echo "[EE] Error in process, [WW] Warning in process, [II] normal Information"
 echo "script was run in directory $folder"
-echo "\n"
+echo ""
 } > $logfile
 ### END LOG FILE HEADER ###
 
@@ -22,21 +21,22 @@ echo "\n"
 echo "start installing programs ..."
 sleep 2
 #install yaourt so all programms can be installed either from AUR
-cd /tmp
+mkdir ~/temp
+cd ~/temp
 wget http://aur.archlinux.org/packages/pa/package-query/package-query.tar.gz
 tar -xzf package-query.tar.gz
 wget http://aur.archlinux.org/packages/ya/yaourt/yaourt.tar.gz
 tar -xzf yaourt.tar.gz
 #do following only when package-query was successfully extracted
-if test -d /tmp/package-query
+if test -d ~/temp/package-query
 then
         cd package-query/
         sudo pacman -S yajl --noconfirm
         makepkg
         sudo pacman -U *.xz --noconfirm
 		#checks if package-query was installed correctly
-		package=$(pacman -Qq package-query)
-		if [ $package == "" ]
+		pacman -Qq package-query
+		if [ $? -eq 1 ]
 		then
 			echo "[EE] package-query was not installed" >> $logfile
 			success=false
@@ -46,14 +46,16 @@ else
 		success=false
 fi
 
-if test -d /tmp/yaourt && $success
+sleep 2
+
+if test -d ~/temp/yaourt && $success
 then
-        cd /tmp/yaourt/
+        cd ~/temp/yaourt/
         makepkg
         sudo pacman -U *.xz --noconfirm
 		#checks if yaourt is installed correctly
-		package=$(pacman -Qq yaourt)
-		if [ $package == "" ]
+		pacman -Qq yaourt
+		if [ $? -eq 1 ]
 		then
 			echo "[EE] yaourt was not installed" >> $logfile
 			success=false
@@ -63,17 +65,21 @@ else
 		success=false
 fi
 
+sleep 2
+
 #intall all packages with yaourt
 if [ $success ]
 then
+	yaourt -Syy
+
 	while read zeile
 	do
 	set $zeile
 			if [ $(echo $zeile | cut -c 1) != "#" ]
 			then
 					yaourt -S $zeile --noconfirm
-					package=$(pacman -Qq $zeile)
-					if [ $package == "" ]
+					pacman -Qq $zeile
+					if [ $? -eq 1 ]
 					then
 							echo "[WW] $zeile was not installed correctly" >> $logfile
 							success=false;
@@ -81,10 +87,12 @@ then
 							echo "[II] $zeile was correctly installed" >> $logfile
 					fi
 			else
-					echo $zeile + " packages are being loaded"
+					echo "$zeile packages are being loaded"
 			fi
 	done < $folder/$1
 fi
+
+sleep 2
 
 #check the status of the installation and print a message when an error occured
 if [ $success ]
@@ -93,7 +101,7 @@ then
 		echo "cleaning up the installation"
 		sleep 2
         sudo pacman -R yaourt package-query yajl
-        rm -r /tmp/{yaourt,package-query}
+        rm -r ~/temp
 		echo "installation cleaned up"
 else
 		echo "... installation finished with error(s)"
