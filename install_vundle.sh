c90263fc147d6cb04922586ad15c62df7dc132f8
Troopa <troopa.der.pate@gmail>
2015-03-17 15:58:00 +0000

    updated install_vundle.sh

--- install_vundle.sh
+++ install_vundle.sh
@@ -7,12 +7,12 @@ then
     echo "directory already exists. continue to install vundle..."
 else
     echo "creating directory ~/.vim/bundle..."
-    mkdir ~/.vim/bundle
+    mkdir -p ~/.vim/bundle
 fi
 
 cd ~/.vim/bundle/
 echo "cloning vundle from github..."
-#git clone https://github.com/gmarik/Vundle.vim.git
+git clone https://github.com/gmarik/Vundle.vim.git
 echo -e "\n______ FINISHED ________\n"
 echo "Vundle is installed now. Check your ~/.vim/bundle directory for a Vundle.vim folder"
 echo -e "use :PluginInstall within vim to install plugins \n"
